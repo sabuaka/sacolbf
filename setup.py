990f5da10865cf33ab0356e7ad5c5d0afa0846f7
sabuaka <sabuaka-fx@hotmail.com>
2018-12-11 15:53:11 +0900

    [fix](ver0.12.2)Fixed an issue with ntplib is not installed.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='sacolbf',
     packages=['sacolbf'],
-    version='0.12.1',
+    version='0.12.2',
     description='Collector Library for bitFlyer',
     author='sabuaka',
     author_email='sabuaka-fx@hotmail.com',
@@ -11,6 +11,7 @@ setup(
     install_requires=[
         'numpy==1.14.4',
         'pandas==0.23.4',
+        'ntplib==0.3.3',
         'sautility@git+https://github.com/sabuaka/sautility.git',
         'sabitflyer@git+https://github.com/sabuaka/sabitflyer.git'
     ],
