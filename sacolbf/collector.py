0c6a8c04cfeb42ff283017757cf20a9e0dacf6c3
sabuaka <sabuaka-fx@hotmail.com>
2019-01-23 20:43:05 +0900

    [mod](ver1.0.0) Remove the watchdog function.

--- sacolbf/collector.py
+++ sacolbf/collector.py
@@ -4,13 +4,11 @@
     broker: bitFlyer
     part: main class
 '''
-import threading
 from enum import IntEnum, auto
 
 from sautility import dt
+from sabitflyer import RealtimeAPI
 
-from .manager import RTAPIManager
-from .manager import MsgEvent
 from .dataset import SADataset
 from .time_adjuster import TimeAdjuster
 
@@ -18,21 +16,15 @@ from .time_adjuster import TimeAdjuster
 class SACollector():
     '''Collector class'''
 
-    __API_STOP_TIMEOUT = 30     # sec
+    __API_PING_INTERVAL = 30    # sec
+    __API_PING_TIMEOUT = 10     # sec
 
-    def __init__(self, event_callback=None, *, debug_mode=False):
-
-        self.__event_callback = event_callback
-
-        self._rtapi_mgr = RTAPIManager(msg_callback=self.__on_msg_event,
-                                       err_callback=self.__on_err_event,
-                                       elog=debug_mode)
-
-        self.__thread_task_listening = None
-
-        self.dataset = SADataset()
-        self.__adjtime = TimeAdjuster.get_singleton()
-        self.__updatetimer_adjtime = -1
+    __LISTEN_CHANNELS = [
+        RealtimeAPI.ListenChannel.BOARD_SNAPSHOT_FX_BTC_JPY,
+        RealtimeAPI.ListenChannel.BOARD_FX_BTC_JPY,
+        RealtimeAPI.ListenChannel.EXECUTIONS_FX_BTC_JPY,
+        RealtimeAPI.ListenChannel.TICKER_FX_BTC_JPY
+    ]
 
     class UpdateEvent(IntEnum):
         '''Change event type'''
@@ -41,42 +33,65 @@ class SACollector():
         TRADE = auto()
         ERROR = auto()
 
-    def __on_msg_event(self, mgr, event, pair, data):  # fixed I/F pylint: disable-msg=W0613
-        up_evt = self.UpdateEvent.ERROR
+    def __init__(self, event_callback=None):
 
+        self.__event_callback = event_callback
+
+        self.__rt_api = self.__create_rtapi()
+
+        self.dataset = SADataset()
+        self.__adjtime = TimeAdjuster.get_singleton()
+        self.__adjtime.update_delta()
+        self.__updatetimer_adjtime = -1
+
+    def __create_rtapi(self):
+        '''create new realtime api instance.'''
+        return RealtimeAPI(self.__LISTEN_CHANNELS,
+                           on_message_board=self.__on_message_board,
+                           on_message_board_snapshot=self.__on_message_board_snapshot,
+                           on_message_ticker=self.__on_message_ticker,
+                           on_message_executions=self.__on_message_executions,
+                           on_error=self.__on_error,
+                           ping_interval=self.__API_PING_INTERVAL,
+                           ping_timeout=self.__API_PING_TIMEOUT)
+
+    def __exec_adjust_time(self):
         if self.__updatetimer_adjtime != dt.get_minute():
             self.__adjtime.update_delta()
             self.__updatetimer_adjtime = dt.get_minute()
 
-        if event == MsgEvent.BOARD_SS:
-            self.dataset.analyze_depth_ss(pair, data)
-            up_evt = self.UpdateEvent.DEPTH
-        elif event == MsgEvent.BOARD_DF:
-            self.dataset.analyze_depth_df(pair, data)
-            up_evt = self.UpdateEvent.DEPTH
-        elif event == MsgEvent.EXECUTIONS:
-            self.dataset.analyze_trade(pair, data)
-            up_evt = self.UpdateEvent.TRADE
-        elif event == MsgEvent.TICKER:
-            self.dataset.analyze_ticker(pair, data)
-            up_evt = self.UpdateEvent.TICK
-
+    def __exec_event_callback(self, event: UpdateEvent):
         if self.__event_callback:
-            self.__event_callback(up_evt, self.dataset)
+            self.__event_callback(event, self.dataset)
 
-    def __on_err_event(self, mgr, ex):  # fixed I/F pylint: disable-msg=W0613
+    def __on_message_board(self, _, pair, data):
+        self.__exec_adjust_time()
+        self.dataset.analyze_depth_df(pair, data)
+        self.__exec_event_callback(self.UpdateEvent.DEPTH)
+
+    def __on_message_board_snapshot(self, _, pair, data):
+        self.__exec_adjust_time()
+        self.dataset.analyze_depth_ss(pair, data)
+        self.__exec_event_callback(self.UpdateEvent.DEPTH)
+
+    def __on_message_ticker(self, _, pair, data):
+        self.__exec_adjust_time()
+        self.dataset.analyze_ticker(pair, data)
+        self.__exec_event_callback(self.UpdateEvent.TICK)
+
+    def __on_message_executions(self, _, pair, datas):
+        self.__exec_adjust_time()
+        self.dataset.analyze_trade(pair, datas)
+        self.__exec_event_callback(self.UpdateEvent.TRADE)
+
+    def __on_error(self, _, ex):
         if self.__event_callback:
-            self.__event_callback(self.UpdateEvent.ERROR, None)
-
-    def __task_proc_listning(self):
-        self._rtapi_mgr.start()
+            self.__event_callback(self.UpdateEvent.ERROR, ex)
 
     def start(self):
         '''Listen start'''
-        self.__thread_task_listening = threading.Thread(target=self.__task_proc_listning)
-        self.__thread_task_listening.start()
+        self.__rt_api.start()
 
     def stop(self):
         '''Listen stop'''
-        self._rtapi_mgr.stop()
-        self.__thread_task_listening.join(timeout=self.__API_STOP_TIMEOUT)
+        self.__rt_api.stop()
